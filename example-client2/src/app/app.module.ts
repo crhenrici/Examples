74418e6c00c51e135a8359b9bc47c1d151aa9ddf
Cristian Henrici <cristian.henrici@prose.one>
2019-03-13 14:37:01 +0100

    angular material design application

--- example-client2/src/app/app.module.ts
+++ example-client2/src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarListComponent } from './car-list/car-list.component';
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- example-server2/src/main/java/com/prose/exampleserver2/controllers/CoolCarController.java
+++ example-server2/src/main/java/com/prose/exampleserver2/controllers/CoolCarController.java
@@ -2,11 +2,14 @@ package com.prose.exampleserver2.controllers;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.prose.exampleserver2.db.*;
 import com.prose.exampleserver2.model.*;
 
+@RestController
 public class CoolCarController {
     private CarRepository repository;
 
@@ -15,6 +18,7 @@ public class CoolCarController {
     }
 
     @GetMapping("/cool-cars")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Car> coolCars() {
         return repository.findAll().stream()
                 .filter(this::isCool)
